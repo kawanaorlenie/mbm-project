f0d0a200543919811b6c1e1a0f9837acfee02ff3
matrom <matrom.java@gmail.com>
2014-12-27 20:34:01 +0100

    encrypted user password

--- mbm-security/src/main/java/pl/mbm/security/configuration/SecurityConfig.java
+++ mbm-security/src/main/java/pl/mbm/security/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package pl.mbm.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import pl.mbm.security.service.impl.UserDetailsServiceImpl;
@@ -19,6 +22,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -41,7 +49,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth)
 			throws Exception {
-		auth.userDetailsService(userDetailsService);
+		auth.userDetailsService(userDetailsService).passwordEncoder(
+				passwordEncoder());
 	}
 
 }
--- mbm-web/src/main/java/pl/mbm/listener/DatabaseFillerOnStartup.java
+++ mbm-web/src/main/java/pl/mbm/listener/DatabaseFillerOnStartup.java
@@ -3,6 +3,7 @@ package pl.mbm.listener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,17 +16,23 @@ public class DatabaseFillerOnStartup implements
 		ApplicationListener<ContextRefreshedEvent> {
 
 	private static final String MATROM_NAME = "matrom";
+	private static final String MATROM_PASSWORD = "password";
 
 	@Autowired
 	private UserDao userDao;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	// brzydkie, ale na razie styknie
 	@Override
 	@Transactional
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		if (userDao.findByName(MATROM_NAME) == null) {
 			User matrom = new UserBuilder().name(MATROM_NAME)
-					.password("password").email("matrom@matrom.pl")
-					.enabled(true).role("ROLE_AMIDN").role("ROLE_USER").build();
+					.password(passwordEncoder.encode(MATROM_PASSWORD))
+					.email("matrom@matrom.pl").enabled(true).role("ROLE_AMIDN")
+					.role("ROLE_USER").build();
 			userDao.save(matrom);
 		}
 
